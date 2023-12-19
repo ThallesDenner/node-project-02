857e5c1c83a5778f7edb96f79fe85c9bd2b23ee5
ThallesDenner <ctdf.engmec@gmail.com>
2023-12-19 19:12:48 -0300

    Realizado configurações para deploy

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,13 @@ import { Knex, knex as setupKnex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true, // isto especifica que todos os campos do banco tem valor null por padrão. O SQLite não tem uma opção para atribuir um valor padrão quando uma coluna é criada. Sem esta linha, aparece um aviso.
   migrations: {
     extension: 'ts',
--- src/env/index.ts
+++ src/env/index.ts
@@ -12,7 +12,8 @@ if (process.env.NODE_ENV === 'test') {
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
+  PORT: z.coerce.number().default(3333),
 })
 
 // O método safeParse realiza tanto a conversão quanto a validação dos dados de entrada (process.env) conforme o esquema definido (envSchema)
